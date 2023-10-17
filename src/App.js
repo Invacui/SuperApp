f6eb27115f374e5201505c4e1c36fc7bfeee8799
Invacui <Ask.72544@gmail.com>
2023-10-17 22:05:50 +0530

    0.1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Components/Signup/signup';
 import PageTwo from './Components/UserSelections/index';
-import Pagethree from './Components/Api_pages/Pagethree';
+//import Pagethree from './Components/Api_pages/Pagethree';
 import Browser from './Components/Browser/Browser';
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
     <Routes>
       <Route exac path="/" element={<Signup />} />
       <Route  path="/userinfo" element={<PageTwo />} />
-      <Route  path="/page3" element={<Pagethree />} />
-      <Route path='/browser' element = {<Browser />}/>
+{/*       <Route  path="/page3" element={<Pagethree />} />
+ */}      <Route path='/browser' element = {<Browser />}/>
     
     </Routes>
   );
